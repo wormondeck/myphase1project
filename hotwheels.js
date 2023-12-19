eb5ac7107c0be65a349545f7584d4637dcf6bb57
Ronney Arias <ronneyarias@gmail.com>
2023-12-19 12:11:29 -0600

    project coding

--- hotwheels.js
+++ hotwheels.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetchAuto()
+    
     function fetchAuto() {
         fetch('http://127.0.0.1:5500/hotwheels.json')
         .then(resp => resp.json())
-        .then(data => console.log(data))
-
+        .then(data => {
+            if (Array.isArray(data.car)) {
+                console.log(data.car)
+                
+            }
+        })
     }
 })
